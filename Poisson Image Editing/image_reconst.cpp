f4d9148a8b3e15cfffe17af2c013edae1e2a802c
Siddharthk <siddharthkherada27@gmail.com>
2012-06-21 09:16:44 +0530

    Copyright notice added

--- Poisson Image Editing/image_reconst.cpp
+++ Poisson Image Editing/image_reconst.cpp
@@ -1,3 +1,22 @@
+/*
+#########################   Poisson Image Editing ############################
+
+Copyright (C) 2012 Siddharth Kherada
+Copyright (C) 2006-2012 Natural User Interface Group
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details. "
+
+##############################################################################
+*/
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
